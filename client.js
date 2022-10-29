ff57bd95fe8501b6e053188c7a3ee629c92fa93d
Billy Salinas <wsalinas315@gmail.com>
2022-10-29 13:26:17 -0500

    added calculateMonthly function and global variable for monthlyCosts

--- client.js
+++ client.js
@@ -6,6 +6,9 @@ let employees = [];
 // unique employee identifier
 let empIdentifier = 0;
 
+// variable for holding current montly payroll costs
+let monthlyCosts =0;
+
 // onReady function
 function onReady(){
     $('#submit-btn').on('click', addWorker);
@@ -29,6 +32,9 @@ function addWorker(){
     // add drone object to the employees array
     employees.push(drone);
 
+    // Call to calculate monthly costs
+    calculateMonthly();
+
     // increment unique employee identifier
     empIdentifier++;
 
@@ -39,39 +45,30 @@ function addWorker(){
     $('#titleEmp').val('');
     $('#salaryEmp').val('');
 
-    console.log(employees);
-
+    //console.log(employees);
     render();
 }
 
 
 // render function
 function render(){
-//console.log('in render function');
+    //console.log('in render function');
+    //empty the existing employee data on the DOM
+    $('#employee-table').empty();
 
-$('#employee-table').empty();
-
-// append worker data to employees table in the DOM
-for(let worker of employees){
-    $('#employee-table').append(`
-        <tr>
-            <td>${worker.firstName}</td>
-            <td>${worker.lastName}</td>
-            <td>${worker.empID}</td>
-            <td>${worker.title}</td>
-            <td>${worker.annualSalary}</td>
-            <td><button class="buttons" id="${worker.uniqueID}">Delete</button></td>
-        </tr>
-    `);
-}
-
-// ^^^ add class for background colors and if statements for index positions????
-
-// append empty bottom row to table
-//$('employee-table').append(`<tr></tr>`);
-
-
-
+    // append worker data to employees table in the DOM
+    for(let worker of employees){
+        $('#employee-table').append(`
+            <tr>
+                <td>${worker.firstName}</td>
+                <td>${worker.lastName}</td>
+                <td>${worker.empID}</td>
+                <td>${worker.title}</td>
+                <td>${worker.annualSalary}</td>
+                <td><button class="buttons" id="${worker.uniqueID}">Delete</button></td>
+            </tr>
+        `);
+    }
 }
 
 // Function to remove a worker
@@ -91,4 +88,14 @@ function removeWorker(){
     employees.splice(employeeIndex, 1);
 
     render();
+}
+
+// function to calculate current monthly payroll costs
+function calculateMonthly(){
+    let tempSum = 0;
+    for(worker of employees){
+        tempSum += Number(worker.annualSalary);
+    }
+    console.log('Total employee salary: ', tempSum);
+    monthlyCosts = tempSum/12;
 }
